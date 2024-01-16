601875dbb752a6f480dc368ba67853020976a061
Jerry Hu <work.jerrywu@gmail.com>
2024-01-16 11:14:53 -0800

    renamed project

--- assets/js/firebase_init.js
+++ assets/js/firebase_init.js
@@ -1,8 +1,8 @@
 var firebaseConfig = {
     apiKey: "AIzaSyCc5IwNHDR8etfNhCiWTat9BJmwXY7v068",
-    authDomain: "thelouwParty.firebaseapp.com",
-    projectId: "thelouwParty",
-    storageBucket: "thelouwParty.appspot.com"
+    authDomain: "thelouwinstitute.firebaseapp.com",
+    projectId: "thelouwinstitute",
+    storageBucket: "thelouwinstitute.appspot.com"
 };
 
 // Initialize Firebase

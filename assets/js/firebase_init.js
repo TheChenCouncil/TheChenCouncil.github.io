c0ffe03fa757b704e41ef82e09bd70fc18831b50
Jerry Hu <work.jerrywu@gmail.com>
2024-01-19 13:13:59 -0800

    Update user authentication logic

--- assets/js/firebase_init.js
+++ assets/js/firebase_init.js
@@ -7,3 +7,8 @@ var firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+window.onload = function () {
+    console.log("%cDON'T MESS AROUND HERE.", 'color:red; font-size: 28px;')
+    console.log("If you know what you are doing, come volunteer with us! https://constellation.iamjerryhu.info/about")
+}

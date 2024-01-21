74c9cf8f06eece3eaaf60fc373cfee697629b42b
Jerry Hu <work.jerrywu@gmail.com>
2024-01-20 18:11:52 -0800

    Refactor code to improve performance and readability

---
+++ assets/js/budget.js
@@ -0,0 +1,11 @@
+function smoothScrollAboveElement(elementId, offset) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        const elementRect = element.getBoundingClientRect();
+        const elementTop = elementRect.top + window.pageYOffset;
+        window.scrollTo({
+            top: elementTop - offset,
+            behavior: 'smooth'
+        });
+    }
+}
